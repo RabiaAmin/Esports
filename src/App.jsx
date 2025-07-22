44e2726332d39b0739e5c0e429c00060da550fb9
RabiaAmin <rabia10march@gmail.com>
2025-07-22 10:16:09 +0200

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-2xl text-red-400 text-center bold uppercase">Initial project setup</h1>
+    </>
+  )
+}
+
+export default App
