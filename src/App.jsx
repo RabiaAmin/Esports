cce64d94f85e5daeb89cfd6633df339788c0f0a3
RabiaAmin <rabia10march@gmail.com>
2025-07-22 18:18:55 +0200

    customize these setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
-
 function App() {
-
-
   return (
     <>
-      <h1 className="text-2xl text-red-400 text-center bold uppercase">Initial project setup</h1>
+    <h1 className=' text-4xl font-bold font-robert-medium uppercase text-center text-primary  '>this is font-robert-medium </h1>
+    <h1 className=' text-4xl font-bold font-robert-regular uppercase text-center text-secondary '>this is  font-robert-regular </h1>
+    <h1 className=' text-4xl font-bold font-circular-web uppercase text-center text-accent'>this is font-circular-web </h1>
+    <h1 className=' text-4xl font-bold font-general uppercase text-center text-foreground '>this is font-general </h1>
+    <h1 className=' text-4xl font-bold font-zentry uppercase text-center text-background'>this is font-zentry </h1>
+    
     </>
+
   )
 }
 
